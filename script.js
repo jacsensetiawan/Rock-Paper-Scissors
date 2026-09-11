1a63c5fc6bc1f7afb3b205a9ed50fe8cc8de6c31
jacsensetiawan <jacsensetsdeveloper@gmail.com>
2026-09-11 20:28:42 +0700

    add getComputerChoice function

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function getComputerChoice(){
     let angka = Math.random();
 
-
+    if (angka >= 0 && angka < 0.333){
+        return "Rock";
+    }else if (angka >= 0.333 && angka < 0.666){
+        return "Paper";
+    } else return "Scissors"
 }
+
