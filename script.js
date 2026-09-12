373a77490fa2d6baeb6e939c2610880b48e23862
jacsensetiawan <jacsensetsdeveloper@gmail.com>
2026-09-12 20:40:11 +0700

    feat: add conditions
    to determine the score and the winner

--- script.js
+++ script.js
@@ -13,4 +13,19 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
  humanChoice=humanChoice.toLowerCase();
+
+ if (humanChoice === computerChoice){
+    console.log ("You Draw! Both chose the same option");
+ } else if (humanChoice === "rock" && computerChoice === "scissors"){
+    console.log ("You Win! Rock beats Scissors");
+    humanScore += 1;
+ } else if (humanChoice === "paper" && computerChoice === "rock"){
+    console.log ("You Win! Paper beats Rock");
+    humanScore += 1;
+ } else if (humanChoice === "rock" && computerChoice === "scissors"){
+    console.log ("You Win! Scissors beats Paper");
+    humanScore += 1;
+ } else {console.log (`You Lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore += 1;
+ }
 };
